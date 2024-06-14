ccf2645c2fb25c4cf2b854e1f898c2b9776ce623
[Carlos Viveros] <[carlos06182016@gmail.com]>
2024-06-13 21:36:25 -0500

    Cambio de nombre carpeta 1.1

--- vite-config.js
+++ vite-config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 
 export default defineConfig({
-base: '/js-vite-backjack/', 
-plugins: []
+base: '/js-vite-backjack/'
 });
